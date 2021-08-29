963ed9789ac3d7be8df572b815fecc4009dbabff
KarimElghamry <Karimelghamry@gmail.com>
2021-08-29 15:10:18 +0200

    add: window implementation to create Canvas's

---
+++ creational/builder/Window.java
@@ -0,0 +1,23 @@
+package creational.builder;
+
+import creational.builder.canvas.Canvas;
+import creational.builder.canvas.CanvasBuilder;
+
+public class Window {
+    private Canvas canvas;
+
+    public void createPlainCanvas(CanvasBuilder builder) {
+        this.canvas = builder.width(200).height(200).rotation(0f).backgroundColor("White").borderType("Solid")
+                .borderColor("Black").build();
+    }
+
+    public void createFunkyCanvas(CanvasBuilder builder) {
+        this.canvas = builder.width(1000).height(300).rotation(420f).backgroundColor("Purple").borderType("Dotted")
+                .borderColor("Pink").build();
+    }
+
+    @Override
+    public String toString() {
+        return this.canvas.toString();
+    }
+}
